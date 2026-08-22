dd9ffcd14e7a261e82288cdcab2b2e5d91121a0d
Shoaib <shoaibchemistrysust@gmail.com>
2026-08-22 21:56:34 +0600

    problem-1 solved

--- src/problem-1.ts
+++ src/problem-1.ts
@@ -7,8 +7,29 @@ Create a function named getTicketPrice. The function should receive the customer
 
  */
 
-function getTicketPrice(age:<type>): <type> {
- 
-    // write your code here
- 
+function getTicketPrice(age: number): number {
+
+    if(age < 5) {
+        return 0;
+    }
+
+    else if (age >= 5 && age <= 12) {
+        return 100;
+    }
+    else if (age >= 13 && age <= 59) {
+        return 200;
+    }
+
+    else {
+
+        return 120;
+
+    }
+
+
 }
+
+console.log(getTicketPrice(3));
+console.log(getTicketPrice(10));
+console.log(getTicketPrice(25));
+console.log(getTicketPrice(65));
