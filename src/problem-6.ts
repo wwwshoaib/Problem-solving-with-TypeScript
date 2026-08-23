ec9450165b4bfc175926b86a103271e7f3ea7108
Shoaib <shoaibchemistrysust@gmail.com>
2026-08-23 15:57:06 +0600

    problem-6 added

---
+++ src/problem-6.ts
@@ -0,0 +1,8 @@
+
+type Role = "admin" | 'editor' | 'viewer';
+
+const canEdit = (role: Role): boolean => ((role === 'admin') || (role === "editor") ) ? true : false;
+
+console.log(canEdit('admin'));
+console.log(canEdit('editor'));
+console.log(canEdit('viewer'));
